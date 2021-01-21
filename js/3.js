e58cc9e7b015317c8482269ac9b683eb340d1043
samir <samircampos2004@gmail.com>
2021-01-21 14:00:34 -0500

    21/01/2021 mi primera version

---
+++ js/3.js
@@ -0,0 +1,54 @@
+
+<!-- Samir Campos -->
+<style>
+#principal{
+background:#55FDFF;width:100%;height:100%
+}
+
+#Login1{
+	background:#2BFF6C;width:500px;height:100px;font-size:25px
+
+}
+#Login2{
+	background:#E72323;width:500px;height:100px;font-size:25px
+
+}
+#Login3{
+	background:#C531F0;width:500px;height:100px;font-size:25px
+
+}
+#Login4{
+	background:#EDFF24;width:500px;height:100px;font-size:25px
+
+}
+#Login5{
+	background:#FF583D;width:500px;height:100px;font-size:25px
+
+}
+#Login6{
+	background:#3324FE;width:500px;height:100px;font-size:25px
+
+}
+	
+</style> 
+
+<div id="principal">
+	 <div id="Login1">
+	 	Samir Campos
+	 </div>
+	  <div id="Login2">
+	 	Samir Campos
+	 </div>
+	  <div id="Login3">
+	 	Samir Campos
+	 </div>
+	  <div id="Login4">
+	 	Samir Campos
+	 </div>
+	  <div id="Login5">
+	 	Samir Campos
+	 </div>
+	  <div id="Login6">
+	    Samir Campos
+	 </div>
+</div>
