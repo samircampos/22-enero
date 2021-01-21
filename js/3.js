b0b7c793f42bf4b9fbeed060bfa8e73baf0c8d4d
samir <samircampos2004@gmail.com>
2021-01-21 14:04:06 -0500

    version 2 mejorada

--- js/3.js
+++ js/3.js
@@ -1,54 +1,24 @@
 
+$(document).on("click","#btn_registrar",function(){
+//validar apellido
+let apellido=$("#txtapellido").val();
+if(apellido.trim().length==0){
+alert("Debe Escribir Un Apellido");
+return false;
+}
+//validar nombre
+let nombre=$("#txtnombres").val();
+if(nombre.trim().length==0){
+alert("Debe Escribir Un Nombre");
+return false;
+}
+alert("Datos Correctos");
+})
+
+let Usuario=$("#txtuser").val();
+if(Usuario.trim().length==0){
+alert("Debe Escribir un Usuario");
+return false;
+}
+alert("Debe escribir un Usuario");
 <!-- Samir Campos -->
-<style>
-#principal{
-background:#55FDFF;width:100%;height:100%
-}
-
-#Login1{
-	background:#2BFF6C;width:500px;height:100px;font-size:25px
-
-}
-#Login2{
-	background:#E72323;width:500px;height:100px;font-size:25px
-
-}
-#Login3{
-	background:#C531F0;width:500px;height:100px;font-size:25px
-
-}
-#Login4{
-	background:#EDFF24;width:500px;height:100px;font-size:25px
-
-}
-#Login5{
-	background:#FF583D;width:500px;height:100px;font-size:25px
-
-}
-#Login6{
-	background:#3324FE;width:500px;height:100px;font-size:25px
-
-}
-	
-</style> 
-
-<div id="principal">
-	 <div id="Login1">
-	 	Samir Campos
-	 </div>
-	  <div id="Login2">
-	 	Samir Campos
-	 </div>
-	  <div id="Login3">
-	 	Samir Campos
-	 </div>
-	  <div id="Login4">
-	 	Samir Campos
-	 </div>
-	  <div id="Login5">
-	 	Samir Campos
-	 </div>
-	  <div id="Login6">
-	    Samir Campos
-	 </div>
-</div>
